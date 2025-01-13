d743d78c4c3ae019872e6cffce2eba5a4c665fac
Pedro Henrique <pedroh280104@gmail.com>
2025-01-13 16:21:05 -0300

    SEGUNDO COMMIT

---
+++ aula36/index.js
@@ -0,0 +1,17 @@
+//const frutas =[ 'banana', 'maçã', 'uva', 'morango', 'abacaxi' ];
+
+const jogadores = {
+    nome: 'Messi',
+    idade: 33,
+    time: 'Barcelona'
+}
+
+for (let chave in jogadores) {
+    console.log(chave, jogadores[chave]);
+}
+// for (let indice in frutas) {
+//    console.log(frutas[indice]);
+//}
+//for (let i = 0; i < frutas.length; i++) {
+//    console.log(frutas[i]);
+//}
